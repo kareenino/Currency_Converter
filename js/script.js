27ba616aa4f2ac10726633c97727cbb69754e547
Kareen <nino.ondanje@gmail.com>
2025-04-23 21:12:26 +0300

    Mock Exchange Rates

--- js/script.js
+++ js/script.js
@@ -1,74 +1,80 @@
-const dropList = document.querySelectorAll(".drop-list select");
-fromCurrency = document.querySelector(".from select");
-toCurrency = document.querySelector(".to select");
-getButton = document.querySelector("form button");
+const mockConversionRates = {
+    USD: 1.0,
+    EUR: 0.8967,
+    JPY: 150.79,
+    GBP: 0.7674,
+    AUD: 1.5946,
+    CAD: 1.4286,
+    CHF: 0.8875,
+    CNY: 7.3046,
+    INR: 85.42,
+    KES: 129.47
+};
 
+const dropList = document.querySelectorAll(".drop-list select"),
+      fromCurrency = dropList[0],
+      toCurrency = dropList[1],
+      exchangeRateTxt = document.querySelector(".exchange-rate"),
+      amountInput = document.querySelector("input");
 
+// Populate dropdowns
 for (let i = 0; i < dropList.length; i++) {
-    for (let currency_code in country_code) {
-        let selected;
-        if(i === 0 && currency_code === "USD"){
-            selected = "selected";
-        } else if(i === 1 && currency_code === "KES"){
-            selected = "selected";
-        } else {
-            selected = "";
-        }
-
+    for (let currency_code in mockConversionRates) {
+        let selected = (i === 0 && currency_code === "USD") || (i === 1 && currency_code === "KES") ? "selected" : "";
         let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
         dropList[i].insertAdjacentHTML("beforeend", optionTag);
     }
 
     dropList[i].addEventListener("change", e => {
         loadFlag(e.target);
+        getExchangeRate(); // update rate immediately when dropdown changes
     });
 }
 
+// Load flag icons
 function loadFlag(element){
-    for(code in country_code){
-        if(code == element.value){
+    for (let code in mockConversionRates){
+        if(code === element.value){
             let imgTag = element.parentElement.querySelector("img");
-            imgTag.src = `https://flagsapi.com/${country_code[code]}/flat/64.png`
+            imgTag.src = `https://flagsapi.com/${country_code[code]}/flat/64.png`; // Using currency code as ISO for mock
         }
     }
 }
 
-window.addEventListener("load", () =>{
-    getExchangeRate();
-});
+// Calculate exchange rate
+function convertCurrency(amount, from, to) {
+    const fromRate = mockConversionRates[from];
+    const toRate = mockConversionRates[to];
 
-getButton.addEventListener("click", e =>{
-    e.preventDefault();//preventing form from submitting
-    getExchangeRate();
-});
+    if (!fromRate || !toRate) return null;
 
-// const exchangeIcon = document.querySelector(".drop-list .icon");
-// exchangeIcon.addEventListener("click", ()=>{
-//     let tempCode = fromCurrency.value;
-//     fromCurrency.value = toCurrency.value;
-//     toCurrency.value  = tempCode;
-//     getExchangeRate();
-// });
+    const usdAmount = amount / fromRate;
+    const converted = usdAmount * toRate;
 
-function getExchangeRate(){
-    const amount = document.querySelector(".amount input");
-    exchangeRateTxt = document.querySelector(".exchange-rate");
-
-    let amountVal = amount.value;
-    //1 will be the default value at all times
-    if(amountVal == "" || amountVal == "0"){
-        amount.value = "1";
-        amountVal = 1;
-    }
-    exchangeRateTxt.innerText = "Getting exchange rate...";
-    let url = `https://v6.exchangerate-api.com/v6/dfd5135b200e567ebbd0205a/latest/${fromCurrency.value}`
-    // let url = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/`
-    fetch(url).then(response => response.json()).then(result =>{
-        let exchangeRate = result.conversion_rates[toCurrency.value]
-        // console.log(exchangeRate);
-        let totalExchangeRate = (amountVal * exchangeRate).toFixed(2);
-        exchangeRateTxt.innerText = `${amountVal} ${fromCurrency.value} = ${totalExchangeRate} ${toCurrency.value}`;
-    }).catch(() => {
-        exchangeRateTxt.innerText = "Something went wrong"
-    });
+    return converted.toFixed(2);
 }
+
+// Get and display exchange rate
+function getExchangeRate() {
+    const amountVal = parseFloat(amountInput.value) || 1;
+
+    exchangeRateTxt.innerText = "Getting exchange rate...";
+
+    const totalExchangeRate = convertCurrency(amountVal, fromCurrency.value, toCurrency.value);
+
+    if (totalExchangeRate !== null) {
+        exchangeRateTxt.innerText = `${amountVal} ${fromCurrency.value} = ${totalExchangeRate} ${toCurrency.value}`;
+    } else {
+        exchangeRateTxt.innerText = "Currency not supported in demo";
+    }
+}
+
+// Update rate when amount changes
+amountInput.addEventListener("input", getExchangeRate);
+
+// On page load
+window.addEventListener("load", () => {
+    loadFlag(fromCurrency);
+    loadFlag(toCurrency);
+    getExchangeRate();
+});
